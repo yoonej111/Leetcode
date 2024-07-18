381224c6999507fd6eade14ffc59b0ed2e8bd857
yoonej111 <52116660+yoonej111@users.noreply.github.com>
2024-07-18 18:02:37 +0900

    Time: 71 ms (20.81%), Space: 56.2 MB (16.47%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -11,22 +11,8 @@
  * @return {boolean}
  */
 var evaluateTree = function(root) {
-    if(!root) return false;
-    
     if(root.val == 0) return false;
-    
-    if(root.val == 1) return true;
-    
-    const leftValue = evaluateTree(root.left)
-    const rightValue = evaluateTree(root.right)
-    
-    if (root.val === 2) {
-        return leftValue || rightValue;
-    } else if (root.val === 3) {
-        return leftValue && rightValue;
-    }
-    
-    
-    
-    return false;
+    else if(root.val == 1) return true;
+    else if(root.val == 2) return evaluateTree(root.left) || evaluateTree(root.right) ;
+    else return evaluateTree(root.left) && evaluateTree(root.right) ;
 };
