fd28e4694e5b1fac5e0aaa60c65ae1fd10495a5e
Eunjae Yoon <yoonej111@gmail.com>
2024-07-31 09:52:49 +0900

    Time: 69 ms (26.09%), Space: 51 MB (32.32%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.js
+++ 3151-special-array-i/3151-special-array-i.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var isArraySpecial = function(nums) {
-    if(nums.array < 2) return true;
-
-    for(let i = 0 ; i < nums.length ; i++){
-        if(nums[i] % 2 == nums[i+1] % 2) return false;
+    if(nums.length == 1) return true;
+    let prev = nums[0] % 2;
+    for(let i = 1 ; i < nums.length ; i++){
+        let current = nums[i] % 2;
+        if(prev == current) return false;
+        prev = current;
     }
-
     return true;
 };
